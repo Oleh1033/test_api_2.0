2114def6643d4c66e328d4f687cfd525dad524c5
Oleh Boburchak <42909793+Oleh1033@users.noreply.github.com>
2019-02-21 09:09:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    var todos = readJSON().map(el => el.description);
-    res.send(todos);
+    res.send('Hello Oleh');
 });
 
 app.get('/api/v1/todos', (req, res) => {
